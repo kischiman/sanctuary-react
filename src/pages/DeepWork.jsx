546c4ca654045424ffafe75d566fbac6701bb557
kischiman <kischi1@online.de>
2026-05-08 17:28:41 +0200

    Fix deepwork static route

--- src/pages/DeepWork.jsx
+++ src/pages/DeepWork.jsx
@@ -1,15 +1,9 @@
+import { useEffect } from 'react'
+
 export default function DeepWork() {
-  return (
-    <iframe
-      src="/deepwork-content.html"
-      title="Deep Work Network"
-      style={{
-        width: '100%',
-        minHeight: '100vh',
-        border: 'none',
-        display: 'block',
-        background: '#fff'
-      }}
-    />
-  )
+  useEffect(() => {
+    window.location.replace('/deepwork/')
+  }, [])
+
+  return null
 }
